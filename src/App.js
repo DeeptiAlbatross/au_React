71e7aa9cce15ca9b3746c5aafe5d33e583b48109
Deepti <2021399693.deepti@ug.sharda.ac.in>
2024-07-02 17:23:36 +0530

    Statics page done

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
       <Contact />
       <Services />
       <Team />
-      {/* <Statics /> */}
+      <Statics />
     </div>
   );
 }
--- src/components/statics.js
+++ src/components/statics.js
@@ -1,13 +1,35 @@
-import React from 'react'
-import Statics from "../assests/statics.png"
+import React from "react";
 
 const statics = () => {
   return (
-    
-    <div className='flex justify-center pb-8 pt-10 bg-gray-100 '>
-        <img  className=" w-2/3 flex justify-center" src={Statics}></img>
+    <div className="pb-20 flex bg-gray-200 pt-6 justify-center flex-row gap-40">
+      <div className="pt-4">
+        <h className="text-blue-800 font-bold text-xl flex justify-start ">Our Statics</h>
+        <div className="pt-6">
+          <span className="text-blue-500 text-4xl font-bold">*Numbers</span>
+          <span>...</span>
+          <span className="text-4xl font-bold">Proves Our Success</span>
+        </div>
+      </div>
+      
+      <div className="flex flex-row gap-20 pt-10">
+      <div class="flex-grow border-t-2 w-[500px] border-blue-600 flex flex-row gap-20">
+        <div className="flex justify-center flex-col pt-4">
+          <h className="text-4xl font-bold">5.2M+</h>
+          <h className="pt-2">Cost savings</h>
+        </div>
+        <div className="flex justify-center flex-col pt-4">
+          <h className="text-4xl font-bold ">75%</h>
+          <h className="pt-2">Profit Boost</h>
+        </div>
+        <div className="flex justify-center flex-col pt-4">
+          <h className="text-4xl font-bold">99.9%</h>
+          <h className="pt-2">Reporting Accuracy</h>
+        </div>
+      </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default statics;
