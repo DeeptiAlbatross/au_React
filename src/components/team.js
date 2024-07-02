50e990902590dcb2a6c23a0aee960094a9b51fbd
Deepti <2021399693.deepti@ug.sharda.ac.in>
2024-07-02 16:18:22 +0530

    Team page made

--- src/components/team.js
+++ src/components/team.js
@@ -1,23 +1,45 @@
 import React from "react";
-import Team from "../assests/Team1.png"
 import { teamData } from "./data";
+import { FaInstagramSquare } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
 
 const team = () => {
   return (
-    <div>
-      <div className="flex justify-center flex-col">
-        <h className="text-blue-800 text-2xl font-bold">Our Expertise team</h>
-        <h1>Your Partners in *Precision</h1>
-        <p className=" text-gray-400">
+    <div className="pb-20">
+      <div className="flex justify-center flex-col pt-20">
+        <h className="text-blue-800 text-xl font-bold p-4">
+          Our Expertise team
+        </h>
+        <h1 className="text-4xl font-bold">Your Partners in *Precision</h1>
+        <p className=" text-gray-400 pt-8">
           AU is pioneering conglomerate,encompassing esteemed firms in the
           realms of Accounting,Audit & Assurance,Consulting, and Legal
-          Services,skilfully <br></br>amalgamating innovative ideas with cutting-edge
-          technology to adeptly tackle complex business challenges.
+          Services,skilfully <br></br>amalgamating innovative ideas with
+          cutting-edge technology to adeptly tackle complex business challenges.
         </p>
       </div>
-      <div className="flex justify-center flex-row gap-4">
-        {teamData?.map((i,index)=>(
-             <img className="w-72" src={i.image}></img>
+      <div className="flex justify-center flex-row gap-7">
+        {teamData?.map((i, index) => (
+          <div key={index}>
+            <img className="w-72 pt-6" src={i.image} alt="#"></img>
+
+            <div className="flex flex-row h-10 w-72  gap-10 ">
+              <div>
+                <h className="text-black font-bold flex pt-3 pl-2 justify-start">{i.title}</h>
+              </div>
+              <div className="flex flex-row pl-32 justify-end gap-1 pt-4">
+                <FaInstagramSquare />
+                <FaFacebook />
+                <FaLinkedin />
+              </div>
+            </div>
+            <div className="flex justify-start">
+            <h className=" pl-2 pb-2">{i.para}</h>
+            </div>
+            <div class="flex-grow border  border-blue-600"></div>
+           
+          </div>
         ))}
       </div>
     </div>
